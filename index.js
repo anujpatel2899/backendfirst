a1fff4cec84520521b040d76a5c8ecf0690d7987
Anuj Patel <anujpatel2899@gmail.com>
2023-08-23 14:48:04 +0200

    First backend project

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const format = require("date-format");
+const app = express();
+const PORT = 4000 || process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.status(201).send("<h1>ANUJ GELLO</h1>");
+});
+
+app.get("/api/v1/facebook", (req, res) => {
+  const Object = {
+    username: "AnujPatelFB",
+    follows: 200,
+    followers: 250,
+    date: format.asString("dd-MM-yy hh:mm:ss.SSS", new Date()),
+  };
+
+  res.status(200).json(Object);
+});
+
+app.get("/api/v1/instagram", (req, res) => {
+  const Object = {
+    username: "AnujPatelInsta",
+    follows: 100,
+    followers: 150,
+    date: format.asString("dd-MM-yy hh:mm:ss.SSS", new Date()),
+  };
+
+  res.status(200).json(Object);
+});
+
+app.get("/api/v1/linkedin", (req, res) => {
+  const Object = {
+    username: "AnujPatelLinkedin",
+    follows: 100,
+    followers: 50,
+    date: format.asString("dd-MM-yy hh:mm:ss.SSS", new Date()),
+  };
+
+  res.status(200).json(Object);
+});
+
+app.get("/api/v1/:token", (req, res) => {
+  console.log(req.params.token);
+  res.status(200).json({ param: req.params.token });
+});
+
+app.listen(PORT, (req, res) => {
+  console.log("Server is Running");
+});
